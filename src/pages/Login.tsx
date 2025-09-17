56c61096b7366170eb716b508a761c7402b2f927
Hadeer Salah <hadeersalah127@gmail.com>
2025-09-17 10:17:20 +0300

    Finishing the login form and some updates

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,17 +4,18 @@ import { motion } from 'framer-motion';
 const Login = () => {
     return (
         <main className="flex justify-between flex-wrap  w-full">
-            <section className="w-[50%]">
+            <section className="w-[50%] flex justify-center items-center">
                 <LoginForm />
             </section>
 
-            <section className="m-0 w-[50%]">
+            <section className="w-1/2 h-full m-0 p-0 flex justify-end items-center">
                 <motion.img
                     src={loginImage}
                     alt="Login Image"
                     initial={{ x: 100, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }}
                     transition={{ duration: 0.8, ease: "easeOut" }}
+                    className="h-full w-full object-cover"
                 />
             </section>
         </main>
