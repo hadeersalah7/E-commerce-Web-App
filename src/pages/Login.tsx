e87f7e4d521d2d320e67c8414f47945be2513d76
Hadeer Salah <hadeersalah127@gmail.com>
2025-09-18 11:29:56 +0300

    Removing the scroll from the login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import loginImage from "../assets/Group 2014.png"
 import { motion } from 'framer-motion';
 const Login = () => {
     return (
-        <main className="flex justify-between flex-wrap  w-full overflow-hidden">
+        <main className="flex w-screen h-screen justify-between flex-wrap overflow-hidden">
             <section className="w-[50%] flex justify-center items-center">
                 <LoginForm />
             </section>
@@ -15,7 +15,6 @@ const Login = () => {
                     initial={{ x: 100, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }}
                     transition={{ duration: 0.8, ease: "easeOut" }}
-                    className="overflow-hidden"
                 />
             </section>
         </main>
