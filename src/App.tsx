a1bb9e32fe3a2de06e0d577644d81d649f5e06b6
Hadeer Salah <hadeersalah127@gmail.com>
2025-09-18 10:01:12 +0300

    adding login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import './App.css'
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
 import { Login } from './pages'
 
 
@@ -7,7 +8,13 @@ function App() {
 
   return (
     <>
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />}>
+          </Route>
+        </Routes>
+      </Router>
+
     </>
   )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,19 +3,19 @@ import loginImage from "../assets/Group 2014.png"
 import { motion } from 'framer-motion';
 const Login = () => {
     return (
-        <main className="flex justify-between flex-wrap  w-full">
+        <main className="flex justify-between flex-wrap  w-full overflow-hidden">
             <section className="w-[50%] flex justify-center items-center">
                 <LoginForm />
             </section>
 
-            <section className="w-1/2 h-full m-0 p-0 flex justify-end items-center">
+            <section className="w-1/2  m-0 p-0 flex justify-end items-center">
                 <motion.img
                     src={loginImage}
                     alt="Login Image"
                     initial={{ x: 100, opacity: 0 }}
                     animate={{ x: 0, opacity: 1 }}
                     transition={{ duration: 0.8, ease: "easeOut" }}
-                    className="h-full w-full object-cover"
+                    className="overflow-hidden"
                 />
             </section>
         </main>
